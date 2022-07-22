15418f2e3d136a87dda42aaea26d523eb5b5c29d
managment_system <mas152q@gmail.com>
2022-07-22 15:33:07 +1000

    graphql connection

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -4,9 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
+import { ApolloProvider, apolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new apolloClient({
+  cache: InMemoryCache,
+  uri: 'http://localhost:3001/graphql',
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
